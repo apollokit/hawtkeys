2309f6df370861bc202d87801d1ca73f0a8247eb
Kit Kennedy <kitkennedy8@gmail.com>
2023-03-04 17:50:49 -0800

    initial version of hotkey click script

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # hotkey_click
+
+A tool for adding hot keys to a UI that doesn't have them. Just clicks in specific 
+coordinates on the screen, so it's pretty bespoke.
+
+hotkey_click.py works under MacOS. You can use it to enable a keyboard event to click somewhere on the screen
+
+use "cmd+shift+4" to get the mouse position in MacOS. It shows the x,y coordinates, 
+
+# Future work
+1. add an app indicator icon that changes state based on the last hotkey acted upon (or create some other ui feedback that hotkey was acted upon)
+1. use a yaml to specify the hotkeys and the x,y coordinates to click
+1. allow hotkeys to use arbitrary modifiers
+1. switch hotkey set based on the program that is focused in the ui
+1. think about a more robust way to sense the location of buttons on the ui (use a screenshot and match pixels with a png?)
---
+++ hotkey_click.py
@@ -0,0 +1,57 @@
+""" Use a hot key to click on the screen on MacOS
+
+Note: use "cmd+shift+4" to get the mouse position. It shows the x,y coordinates, 
+ the bookwhich can be used with pyautogui.click(x,y)
+
+A lot of this code came from keyboard.py in better_dictate, 
+"""
+from typing import Union
+import time
+
+import pyautogui
+from pynput import keyboard
+
+class KeyboardManager():
+
+    def on_press(self, key: keyboard.KeyCode):
+        # if str(key) in keys_for_parser:
+        #     # print("setting key_pressed_parser_event")
+        #     key_pressed_parser_event.set()
+        pass
+
+
+    def on_release(self,
+        key: Union[keyboard.KeyCode, keyboard.Key]):
+        """ Takes action upon key release.
+
+        Args:
+            key: the key that was released
+        """
+        keystring = str(key).strip('\'')
+
+        ## for kicad UI buttons
+
+        ## pcbnew
+        # these work on my hp monitor at home, with the pcbnew window maximized
+        
+        # alt+b -> button for "show filled areas in zones"
+        if keystring == '∫':
+            print(f'Saw {keystring} hotkey, show filled areas in zones')
+            pyautogui.click(16,400)
+        # alt+n -> button for "do not show filled areas in zones"
+        elif keystring == '˜':
+            print(f'Saw {keystring} hotkey, do not show filled areas in zones')
+            pyautogui.click(16,435)
+
+if __name__ == '__main__':
+
+    kb_mngr = KeyboardManager()
+
+    keyb_listener = keyboard.Listener(
+        on_press=kb_mngr.on_press,
+        on_release=kb_mngr.on_release)
+
+    keyb_listener.start()
+
+    while True:
+        time.sleep(1)
